b25e4367421340df1f311c545aea8d3ad69268aa
Mateus Dantas <mateusdantas.dev@gmail.com>
2023-06-15 00:23:00 -0300

    Cria tabela categoria

---
+++ src/main/resources/db/migration/V1__create_category_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE categoria (
+    id BIGINT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(50) NOT NULL
+);
+
+INSERT INTO categoria (nome) VALUES ('Alimentação');
+INSERT INTO categoria (nome) VALUES ('Cartão');
+INSERT INTO categoria (nome) VALUES ('Luz');
+INSERT INTO categoria (nome) VALUES ('Farmácia');
+INSERT INTO categoria (nome) VALUES ('Supermercado');
+INSERT INTO categoria (nome) VALUES ('Internet');
+INSERT INTO categoria (nome) VALUES ('Transporte');
---
+++ src/test/java/br/com/demtech/ApiControleGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
